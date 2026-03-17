3c3a875fe65b47d85935c3cece4faaaae22ad69b
EddyTHK <115358176+EddyTHK@users.noreply.github.com>
2026-03-17 16:02:53 +0800

    Update bank files

---
+++ src/simscli/simscli/bank/SimpleInterestPolicy.java
@@ -0,0 +1,10 @@
+package simscli.simscli.bank;
+
+public class SimpleInterestPolicy implements InterestPolicy {
+    private static final double RATE = 0.0005;
+
+    public Money calculateInterest(Money balance) {
+        int interest = (int)(balance.getAmount() * RATE);
+        return new Money(interest);
+    }
+}
